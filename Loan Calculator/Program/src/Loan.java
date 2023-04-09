e9cae090ee5aae15aa7e1dba8d2c7bcca8941733
Mona Lisa <slavi.nedqlkov@abv.bg>
2023-04-09 19:50:06 +0300

    Finished Project, Added Some Documentation

--- Loan Calculator/Program/src/Loan.java
+++ Loan Calculator/Program/src/Loan.java
@@ -1,12 +1,18 @@
- import java.text.DecimalFormat;
+import java.text.DecimalFormat;
 
+/*
+This program shows how much a loan will cost.
+It takes the principal and applies the specified interest rate,
+it can also calculate the cost of each monthly payment (with interest rate).
+Example: 50,000$ loan with 6% interest and 3-year loan term will:
+        Cost: 4756$
+        Require you to pay(each month): 1521$
+
+*/
 public class Loan {
         double loanAmount;
-        double interestRate;
-        double lengthOfLoan;
-        double total;
-        double totalIntRate;
-        double monthlyPayment;
+        double interestRate; //in terms of percents
+        double lengthOfLoan; //in terms of months
         double totalMonthlyPayment;
 
         public Loan (double principal, double intRate, double loanTerm) {
@@ -15,6 +21,7 @@ public class Loan {
             lengthOfLoan = loanTerm;
         }
 
+            //Calculates how much will each monthly payment be
         public double totalMonthlyDue () {
             double monthlyInterest = interestRate / 12;
             double calc1 = 1 + monthlyInterest;
@@ -22,10 +29,11 @@ public class Loan {
             return totalMonthlyPayment = Math.round(loanAmount * ((monthlyInterest * exponentCalculation) / (exponentCalculation - 1)));
         }
 
-
         public static void main(String[] args) {
-            Loan carLoan = new Loan(30000, 3, 48);
+            Loan carLoan = new Loan(100000, 6, 12);
             double monthlyPayment = carLoan.totalMonthlyDue();
-            System.out.println(monthlyPayment);
+            System.out.println("Monthly Repayment will be: " + monthlyPayment + "$");
+            System.out.println("Total amount of the loan will be: " + Math.round(monthlyPayment * carLoan.lengthOfLoan) + "$");
+            System.out.println("Therefore the loan will cost: " + Math.round((monthlyPayment * carLoan.lengthOfLoan) - carLoan.loanAmount) + "$");
         }
     }
