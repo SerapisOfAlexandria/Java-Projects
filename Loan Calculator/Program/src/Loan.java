ce78e163f269b41fc404841b0287f97bb2246a0a
Mona Lisa <slavi.nedqlkov@abv.bg>
2023-04-09 10:35:53 +0300

    Added Instance Fields

--- Loan Calculator/Program/src/Loan.java
+++ Loan Calculator/Program/src/Loan.java
@@ -1,4 +1,9 @@
 public class Loan {
+    double principial;
+    double interestRate;
+    double loanTerm; // in terms of years
+    double loanPayments; // the amout of money that must be paid in every time interval
+
 
     public Loan() {
 
