093cff1edfc5d49afce4a550e8b234a26edfb341
Mona Lisa <slavi.nedqlkov@abv.bg>
2023-04-09 19:27:17 +0300

    Rewrote Everything & Made More Effective

--- Loan Calculator/Program/src/Loan.java
+++ Loan Calculator/Program/src/Loan.java
@@ -1,52 +1,31 @@
+ import java.text.DecimalFormat;
+
 public class Loan {
-    double principial;
-    double interestRate;
-    double loanTerm; // in terms of months
-    double loanPayments; // the amount of money that must be paid in every time interval
-    double downPayment; // The amount of money that can be deposited initially plus the loan payments
-   /*
-            How the program should work:
+        double loanAmount;
+        double interestRate;
+        double lengthOfLoan;
+        double total;
+        double totalIntRate;
+        double monthlyPayment;
+        double totalMonthlyPayment;
 
-       Let's say someone loans a 100,000$ loan with 4% interest rate, 12-month loan term, and one-half down payment,
-       paid in the first half of the total term.
-       The program should be able to calculate the total cost of the loan(loan + interest rate), how much each loanPayment
-       should be. Example:
+        public Loan (double principal, double intRate, double loanTerm) {
+            loanAmount = principal;
+            interestRate = intRate / 100;
+            lengthOfLoan = loanTerm;
+        }
+
+        public double totalMonthlyDue () {
+            double monthlyInterest = interestRate / 12;
+            double calc1 = 1 + monthlyInterest;
+            double exponentCalculation = Math.pow(calc1, lengthOfLoan);
+            return totalMonthlyPayment = Math.round(loanAmount * ((monthlyInterest * exponentCalculation) / (exponentCalculation - 1)));
+        }
 
 
-
-     */
-
-
-    public Loan(double loanAmount, double lenghtOfLoan, double initialDeposit) {
-        principial = loanAmount;
-        loanTerm = lenghtOfLoan;
-        downPayment = initialDeposit;
-
-    }
-            // Interest rate calculator
-    public void intRateCalculator() {
-        if (loanTerm <= 12) {
-            if (downPayment >= downPayment / 2 || downPayment >= downPayment / 3) {
-                interestRate = .4;
-            } else {
-                interestRate = .45;
-            }
-        } else if (loanTerm <= 36) {
-            if (downPayment >= downPayment / 2 || downPayment >= downPayment / 3 || downPayment >= downPayment / 4) {
-                interestRate = .5;
-            } else {
-                interestRate = .55;
-            }
-        } else {
-            if (downPayment >= downPayment / 2 || downPayment >= downPayment / 3 || downPayment >= downPayment / 4) {
-                interestRate = .6;
-            } else {
-                interestRate = .65;
-            }
+        public static void main(String[] args) {
+            Loan carLoan = new Loan(30000, 3, 48);
+            double monthlyPayment = carLoan.totalMonthlyDue();
+            System.out.println(monthlyPayment);
         }
     }
-
-    public static void main(String[] args) {
-
-    }
-}
