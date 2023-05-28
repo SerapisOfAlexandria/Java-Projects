9605377b80b242851098f45940b0e8eab81b9a6b
Mona Lisa <slavi.nedqlkov@abv.bg>
2023-05-28 11:30:17 +0300

    Homework

--- SU02ConditionalStatements/src/Homework/EvenOrOdd.java
+++ SU02ConditionalStatements/src/Homework/EvenOrOdd.java
@@ -1,7 +1,18 @@
 package Homework;
 
+import java.util.Scanner;
+
 public class EvenOrOdd {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int num1 = Integer.parseInt(input.nextLine());
+
+        if (num1 % 2 == 0) {
+            System.out.println("even");
+        } else {
+            System.out.println("odd");
+        }
 
     }
 }
