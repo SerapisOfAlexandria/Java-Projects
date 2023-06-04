66d2ac6f65d83469fbd179e20b9a70f0f926435c
Mona Lisa <slavi.nedqlkov@abv.bg>
2023-06-04 11:29:56 +0300

    Homework

---
+++ SU04ForLoops/src/Homework/EvenPowersOf2.java
@@ -0,0 +1,17 @@
+package Homework;
+
+import java.util.Scanner;
+
+public class EvenPowersOf2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int number = Integer.parseInt(input.nextLine());
+
+        for(int i = 0; i <= number; i++) {
+            if (i % 2 == 0) {
+                System.out.println((int) Math.pow(2, i));
+            }
+        }
+    }
+}
