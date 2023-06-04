21e01c5120dade75504a12ba0da833f02c392d41
Mona Lisa <slavi.nedqlkov@abv.bg>
2023-06-04 11:39:49 +0300

    Homework

---
+++ SU04ForLoops/src/Homework/CharactersSequence.java
@@ -0,0 +1,15 @@
+package Homework;
+
+import java.util.Scanner;
+
+public class CharactersSequence {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String text = input.nextLine();
+
+        for(int i = 0; i <= text.length() - 1; i++) {
+            System.out.println(text.charAt(i));
+        }
+    }
+}
