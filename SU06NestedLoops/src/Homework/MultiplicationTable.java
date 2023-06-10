65bda251077d837e0ab5c1438f9a0eea0b65db36
Mona Lisa <slavi.nedqlkov@abv.bg>
2023-06-10 21:35:42 +0300

    Homework

--- SU06NestedLoops/src/Homework/MultiplicationTable.java
+++ SU06NestedLoops/src/Homework/MultiplicationTable.java
@@ -4,21 +4,11 @@ import java.util.Scanner;
 
 public class MultiplicationTable {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        int n = Integer.parseInt(input.nextLine());
-        int validCombinations = 0;
-
-        for (int x1 = 0; x1 <= n; x1++) {
-            for (int x2 = 0; x2 <= n; x2++) {
-                for (int x3 = 0; x3 <= n; x3++) {
-                    if (x1 + x2 + x3 == n) {
-                        validCombinations++;
-                        System.out.printf("%d %d %d\n", x1, x2, x3);
-                    }
-                }
+        for (int i = 1; i <= 10; i++) {
+            for (int b = 1; b <= 10; b++) {
+                System.out.printf("%d * %d = %d\n", i, b, i * b);
             }
         }
-        System.out.println(validCombinations);
+
     }
 }
