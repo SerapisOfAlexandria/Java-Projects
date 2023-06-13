ea317e6c6f3c1ec55f3774a5db75da7f025031bb
Mona Lisa <slavi.nedqlkov@abv.bg>
2023-06-13 21:21:44 +0300

    Homework

--- SU06NestedLoops/src/Homework/SumBetweenTwoNumbers.java
+++ SU06NestedLoops/src/Homework/SumBetweenTwoNumbers.java
@@ -10,23 +10,15 @@ public class SumBetweenTwoNumbers {
         int numberRangeTwo = Integer.parseInt(input.nextLine());
         int number = Integer.parseInt(input.nextLine());
         int combinations = 0;
-        int breakLoop = 0;
 
-        for (int i = numberRangeOne; i <= numberRangeTwo; i++) {
-            if (breakLoop == 1) {
-                break;
-            }
-            for (int b = numberRangeOne; b <= numberRangeTwo; b++) {
-                combinations++;
-                if (i + b == number) {
-                    breakLoop++;
-                    System.out.printf("Combination N:%d (%d + %d = %d)", combinations, i, b, number);
-                    break;
+            for (int i = numberRangeOne; i <= numberRangeTwo; i++) {
+                for (int b = numberRangeOne; b <= numberRangeTwo; b++) {
+                    combinations++;
+                    if (i + b == number) {
+                        System.out.printf("Combination N:%d (%d + %d = %d)", combinations, i, b, number);
+                        return;
+                    }
                 }
-            }
-        }
-        if (breakLoop == 0) {
-            System.out.printf("%d combinations - neither equals %d", combinations, number);
         }
     }
 }
