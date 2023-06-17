90bcf2e5fa6f1baaaacf00ab95b340e7cade7b57
Mona Lisa <slavi.nedqlkov@abv.bg>
2023-06-17 07:51:51 +0300

    Exercise1

---
+++ HundredAndFiftyExercises/src/Exercise1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        System.out.printf("Hello \n%s", name);
+
+    }
+}
