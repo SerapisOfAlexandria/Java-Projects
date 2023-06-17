0b9f7767bab5330d606787d259734118581727ef
Mona Lisa <slavi.nedqlkov@abv.bg>
2023-06-17 07:55:33 +0300

    Exercise2

---
+++ HundredAndFiftyExercises/src/Exercise3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Exercise3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int x = Integer.parseInt(input.nextLine());
+        int y = Integer.parseInt(input.nextLine());
+
+        System.out.println(x / y);
+    }
+}
